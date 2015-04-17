e0795c681174c893070f3b667f6e7d3f50208848
Michael <mding5692@users.noreply.github.com>
2015-04-17 17:19:18 -0400

    Create exercise1.s
    First exercise

---
+++ exercise1.s
@@ -0,0 +1,13 @@
+  ; Exercise: Scans string terminated by null byte (0x00) and copies sting 
+  ; from source location pointed to by r0 to destination pointed by r1
+  
+    area exercise1, code, readonly
+    entry
+    
+    
+    
+    
+    area exercise1, data, readwrite
+    
+    
+    end
