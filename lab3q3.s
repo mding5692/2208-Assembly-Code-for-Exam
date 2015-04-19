bbe8bea89255e6a9c27d0ffae32ba3aa252ad95d
Michael <mding5692@users.noreply.github.com>
2015-04-19 02:50:47 -0400

    Update lab3q3.s

--- lab3q3.s
+++ lab3q3.s
@@ -4,30 +4,30 @@
 q3a		mov 	r0, #3
 		mov 	r1, #4		; assigns dummy values for testing
 		
-		cmp 	r0, r1		; compares the two
-loop1	cmp 	r0, r1
-		beq		done
-		bgt 	gr8r1
-		blt		lesr1
-		
-gr8r1	sub 	r0, r0, r1
-		b		loop1
 
-lesr1	sub 	r1, r1, r0
-		b 		loop1
+loop1		cmp 	r0, r1
+		beq	done
+		bgt 	gr8r1
+		blt	lesr1
+		
+gr8r1		sub 	r0, r0, r1
+		b	loop1
+
+lesr1		sub 	r1, r1, r0
+		b 	loop1
 	
 done	
 		;=========================================
 q3b		mov 	r0, #3
 		mov 	r1, #4
 		
-loop2	cmp 	r0, r1
-		beq		done2
+loop2		cmp 	r0, r1
+		beq	done2
 		subgt 	r0,r0,r1
 		sublt	r1,r1,r0
-		b		loop2
+		b	loop2
 		
-done2	b 		done2
+done2		b 	done2
 
 		
 		end
