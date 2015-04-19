17c38940f64fcd1648055a75c57b3f14dc90dfbd
Michael <mding5692@users.noreply.github.com>
2015-04-19 14:05:38 -0400

    Update lab4q3.s

--- lab4q3.s
+++ lab4q3.s
@@ -9,26 +9,26 @@
 			
 			; boilerplate code: points r0 to a and r1 to endOfArray
 			adr		r0, a
-			adr 	r1, endOfArray
+			adr 		r1, endOfArray
 			adr		sp, stack		; points to stack, maybe wrong, i need to do better 									;with stacks
 			mov		r3, #0			; uses r3 as a count
 			; loads the values into the stack
-load			add		r3,	r3, #4		; r3 is incremented
-			ldr		r4,	[a, r3]!	; loads into r2 whatever is in the array a
-			str		r4,	[sp, r3]	; stores it into the stack
+load			add		r3, r3, #4		; r3 is incremented
+			ldr		r4, [a, r3]!	; loads into r2 whatever is in the array a
+			str		r4, [sp, r3]	; stores it into the stack
 			cmp		r4, r1			; compares the two if equal the loading is done
 			beq		startloop		; starts the comparison loop
 			bne		load			; if not equal loops through loading
 			; starts loop to iterate through stack
-main			ldr		r3,	[sp,#4]!	; sp is incremented and loaded into r3	
+main			ldr		r3, [sp,#4]!	; sp is incremented and loaded into r3	
 			cmp		r3, endOfArray	; if not equal to endOfArray, keep looping
 			beq		done			; if it is, its equal
 			bne		sortTwo			; calls the sorting function
 			
-sortTwo			ldr		r4,	[sp,#4]		; r4 is sp + 4 or else incremented by 1 word
+sortTwo			ldr		r4,[sp,#4]		; r4 is sp + 4 or else incremented by 1 word
 			cmp		r4, r3			; compares r4 and r3
 			bgt		endSort			; if r4 is greater than r3, end the sort
-			str		r4,	[sp]		; stores r4 at where the sp is pointing at, aka the lesser element
+			str		r4, [sp]		; stores r4 at where the sp is pointing at, aka the lesser element
 			str		r3, [sp,#4]		; stores r3 where r4 was at	
 
 endSort			b		main			; when done, goes back to the Loop
