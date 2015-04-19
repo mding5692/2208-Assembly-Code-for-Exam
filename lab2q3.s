61c6cd1df16b7d3576f81fbbd8d08be88a332ca3
Michael <mding5692@users.noreply.github.com>
2015-04-19 01:38:17 -0400

    Create lab2q3.s

---
+++ lab2q3.s
@@ -0,0 +1,17 @@
+		  area lab2q3and4, code, readonly
+		  entry
+		
+		  mov r4, #2	; gives r4 dummy value
+q3		add	r0, r4, r4, lsr #128	
+		
+		  b 	done
+		
+q4a		;	0000 00 0 0000 0 0010 0011 111111110000	; should be that but pls correct me
+q4b		;   confused with this one, need help
+
+q5		;	
+		
+
+      done	
+		
+		  end
