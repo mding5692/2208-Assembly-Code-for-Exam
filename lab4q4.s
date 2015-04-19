7f07ef2483ec69a331ba7f466a74838739875e5e
Michael <mding5692@users.noreply.github.com>
2015-04-19 14:03:49 -0400

    Update lab4q4.s

--- lab4q4.s
+++ lab4q4.s
@@ -8,17 +8,17 @@
 			mov		r3, #0			; uses r3 as a count
 			mov		r6, #0			; uses r6 as a count
 			; loads the values into the stack
-load		add		r3,	r3, #4		; r3 is incremented
+load			add		r3,	r3, #4		; r3 is incremented
 			ldr		r4,	[a, r3]!	; loads into r2 whatever is in the array a
 			str		r4,	[sp, r3]	; stores it into the stack
 			cmp		r4, r1			; compares the two if equal the loading is done
 			beq		startloop		; starts the comparison loop
 			bne		load			; if not equal loops through loading
 			; starts loop to iterate through stack
-main		ldr		r4,	[sp,#4]!	; sp is incremented and loaded into r3	
+main			ldr		r4,	[sp,#4]!	; sp is incremented and loaded into r3	
 			b		sortTwo			; calls the sorting function
 			
-sortTwo		ldr		r5,	[sp,#4]		; r4 is sp + 4 or else incremented by 1 word
+sortTwo			ldr		r5,	[sp,#4]		; r4 is sp + 4 or else incremented by 1 word
 			cmp		r5, endOfArray	; if not equal to endOfArray, keep looping
 			beq		sortUsed		; sees if sort has been used
 			cmp		r5, r4			; compares r5 and r4
@@ -28,19 +28,19 @@ sortTwo		ldr		r5,	[sp,#4]		; r4 is sp + 4 or else incremented by 1 word
 			str		r4, [sp,#4]		; stores r3 where r4 was at	
 			b		main			; moves back to main
 
-endSort		sub		r6,r6,#1		; if sort is not needed then decrement
+endSort			sub		r6,r6,#1		; if sort is not needed then decrement
 			b		main
 			
-sortUsed	cmp		r6, #0			; compares r5 with 0
+sortUsed		cmp		r6, #0			; compares r5 with 0
 			beq		done			; if there is no errors or it has been solved 
 			bgt		main				; keeps iterating if there are still issues
 			
 
-done		b		done
+done			b		done
 				
 			area 	lab4q3, data, readwrite
-a          	dcd 	44,-56,3,65,-8,32,6,-87,54,65,87,32,65 
-endOfArray 	space 	1 
+a          		dcd 	44,-56,3,65,-8,32,6,-87,54,65,87,32,65 
+endOfArray 		space 	1 
 
-stack		space	#0x00		; empty stack
+stack			space	#0x00		; empty stack
 			end
