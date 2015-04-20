031b1611d3d35d8722b361a74f421fa0e9ab4337
Michael <mding5692@users.noreply.github.com>
2015-04-20 16:24:16 -0400

    Update lab2q2.s

--- lab2q2.s
+++ lab2q2.s
@@ -1,30 +1,26 @@
         	    area lab2q2, code, readonly
 		    entry
 		
-		    ; assigns dummy values to test operations
+		    ; assigns dummy values to test operations -- i guess these this doesnt count
 		    mov r0, #4
 		    mov	r1, #2
 		    mov r2, #3
 		    mov r4, #0		; clears r4 just in case
+		    
+		    
 main	  	    tst	r0, #1	; if it is even it is equal to zero
-		    beq	isEven
-		    bne	isOdd
 		
-isEven		    mov	r1, r1, lsr r0
-		    mov r2, r2, lsl r0
+isEven		    moveq r1, r1, lsr r0
+		    moveq r2, r2, lsl r0
+		    beq	result
 		
-		    b	result
+isOdd	  	    movne r1, r1, lsl #8
+		    movne r2, r2, lsr #8
 		
-isOdd	  	    mov r1, r1, lsl #8
-		    mov r2, r2, lsr #8
+result		    add r2, r3, lsr #4	; divides by 16
+		    add r4, r1, r2 	
 		
-		    b 	result
-		
-result		    mov r3, r3, lsl #4	; divides by 16
-		    add	r2, r2, r3
-		    add r4, r1, r2	
-		
-	    	    area lab2q2, data, readwrite
+	    
 		
 		
 		
