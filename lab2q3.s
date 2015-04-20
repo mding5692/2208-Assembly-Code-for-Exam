1c189f9b5777555a6624e1dc45a082502515ee77
Michael <mding5692@users.noreply.github.com>
2015-04-20 17:25:50 -0400

    Update lab2q3.s

--- lab2q3.s
+++ lab2q3.s
@@ -6,8 +6,8 @@ q3		  add	r0, r4, r4, lsr #128
 		
 		  b 	done
 		
-q4a		;	0000 00 0 0000 0 0010 0011 111111110000	; should be that but pls correct me
-q4b		;   confused with this one, need help
+q4a		;	1110 00 0 0000 0 0011 0010 111111110000	; = 0xE0032FF0 should be that but pls correct me
+q4b		;   	binary = 0101 0000 0000 0111 0110 1000 0000 1000 => ANDPL r6, r7, #808
 
 q5		;	
 		
