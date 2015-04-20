5ae0c3dbf87d7dc318c2731ebe678643318fe901
Michael <mding5692@users.noreply.github.com>
2015-04-20 18:16:27 -0400

    Update lab2q3.s

--- lab2q3.s
+++ lab2q3.s
@@ -9,7 +9,7 @@ q3		  add	r0, r4, r4, lsr #128
 q4a		;	1110 00 0 0000 0 0011 0010 111111110000	; = 0xE0032FF0 should be that but pls correct me
 q4b		;   	binary = 0101 0000 0000 0111 0110 1000 0000 1000 => ANDPL r6, r7, #808
 
-q5		;	
+q5		;	binary = 1110 0110 1101 1100 1010 0100 0000 1000 => LDRB 
 		
 
                   done	
